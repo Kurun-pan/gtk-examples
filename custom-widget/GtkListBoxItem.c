34143b6736159540f512cad40536b275ef93097f
kurun-pan <robo_matsu_rhhqd142@yahoo.co.jp>
2019-12-31 16:24:28 +0900

    added custom widget sample

---
+++ custom-widget/GtkListBoxItem.c
@@ -0,0 +1,50 @@
+#include <gtk/gtk.h>
+#include "GtkListBoxItem.h"
+
+/**
+ *[refs]: https://developer.gnome.org/gtk3/stable/GtkWidget.html
+ *        Building composite widgets from template XML
+ */
+G_DEFINE_TYPE_WITH_PRIVATE (GtkListBoxItem, gtk_listbox_row, GTK_TYPE_LIST_BOX_ROW);
+
+static void gtk_listbox_row_class_init (GtkListBoxItemClass *klass)
+{
+    gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS(klass),
+                                                 GTK_LISTBOX_ITEM_CLASS_DEFINITION_FILE);
+
+    gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS(klass),
+                                                  GtkListBoxItem, label_no);
+    gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS(klass),
+                                                  GtkListBoxItem, label_body);
+
+    //gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(klass), xxx);
+}
+
+static void gtk_listbox_row_init (GtkListBoxItem *self)
+{
+    self->priv = (GtkListBoxItemPrivate *) gtk_listbox_row_get_instance_private (GTK_LISTBOX_ITEM (self));
+    gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+GtkListBoxItem *gtk_listbox_item_new (gint index, const gchar *str)
+{
+    GtkListBoxItem *row = 
+        GTK_LISTBOX_ITEM (g_object_new (gtk_listbox_row_get_type (), NULL));
+
+    g_warn_if_fail (row != NULL);
+    g_warn_if_fail (row->priv != NULL);
+    g_warn_if_fail (row->priv->label_body != NULL);
+
+    /* リストビューのNoを設定 */
+    {
+        gchar idx[16];
+        memset (idx, 0x0, 16);
+        sprintf (idx, "%d", index);
+        gtk_label_set_text (row->priv->label_no, idx);
+    }
+
+    /* リストビューの文字を設定 */
+        gtk_label_set_text (row->priv->label_body, str);
+
+    return row;
+}
---
+++ custom-widget/custom_widget.c
@@ -0,0 +1,74 @@
+#include <gtk/gtk.h>
+#include "GtkListBoxItem.h"
+
+static gboolean cb_item_selected (GtkListBox *listbox, GtkListBoxItem *row, gpointer user_data)
+{
+    if (row != NULL) {
+        const gint index = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (row));
+        g_print ("item selected -> No.%d %s\n", index, gtk_label_get_text (row->priv->label_body));
+    }
+
+    return TRUE;
+}
+
+static void cb_app_activate (GtkApplication* app, gpointer user_data)
+{
+    GtkBuilder* builder = gtk_builder_new ();
+    g_return_if_fail (builder != NULL);
+
+    /* gladeファイルから画面情報を読み込み */
+    GError* error = NULL;
+    if (!gtk_builder_add_from_file (builder, "./res/main.glade", &error)) {
+        if (error) {
+            g_error ("Failed to load: %s", error->message);
+            g_error_free (error);
+            return ;
+        }
+    }
+
+    GtkApplicationWindow *window = GTK_APPLICATION_WINDOW (gtk_builder_get_object (builder, "application_window"));
+    g_warn_if_fail (window != NULL);
+    g_object_set (window, "application", app, NULL);
+
+    /* GtkLixtBox設定 */
+    GtkWidget *listbox = GTK_WIDGET (gtk_builder_get_object (builder, "listbox"));
+    g_warn_if_fail (listbox != NULL);
+
+    /* リストアイテム選択時のイベントを登録 */
+    g_signal_connect (listbox, "row-selected", G_CALLBACK (cb_item_selected), NULL);
+
+    /* アイテムをセット */
+    for (int i = 0; i < 10; i++) {
+        static const gchar items[][256] = {
+            "寿司",
+            "ステーキ",
+            "カレーライス",
+            "ハンバーグ",
+            "冷やし中華",
+            "うどん",
+            "唐揚げ",
+            "そば",
+            "チャーハン",
+            "海鮮丼",
+        };
+        GtkListBoxItem *item = gtk_listbox_item_new (i, items[i]);
+        gtk_container_add (GTK_CONTAINER (listbox), GTK_WIDGET (item));
+    }
+
+    gtk_widget_show_all (GTK_WIDGET (window));
+    g_object_unref (builder);
+}
+
+int main (int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk3.custom-widget", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (cb_app_activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
+}
+
