9ffcc8b23937ac2522fd9c9c90ecef851fd5061c
kurun-pan <robo_matsu_rhhqd142@yahoo.co.jp>
2020-01-18 13:56:19 +0900

    Add comments

--- webview/main.c
+++ webview/main.c
@@ -16,17 +16,23 @@ static gint cb_command_line (GtkApplication* app, GApplicationCommandLine *comma
     gchar **argv;
     gint argc;
 
+    /* コマンドライン引数情報を取得 */
     argv = g_application_command_line_get_arguments (command_line, &argc);
     if (argc == 2) {
+        /* グローバル変数に保存 */
         sprintf (s_url, "%s", argv[1]);
+
+        /* 取得したコマンドライン引数の開放処理が必須です */
         g_strfreev (argv);
+
+        /* GtkApplicationをアクティベート */
         g_application_activate (G_APPLICATION (app));
         return 0;
     }
     else
       g_print ("Usage: webkit_sample URL\n");
 
-    return 2;
+    return argc;
 }
 
 static void cb_activate (GtkApplication* app, gpointer user_data)
@@ -34,7 +40,6 @@ static void cb_activate (GtkApplication* app, gpointer user_data)
     GtkWidget *window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Webviewサンプル");
     gtk_window_set_default_size (GTK_WINDOW (window), 1024, 768);
-    //gtk_window_fullscreen (GTK_WINDOW (window));
 
     /**
      * WebKitGTK Reference Manual for WebKitGTK 2.26.2
@@ -46,16 +51,17 @@ static void cb_activate (GtkApplication* app, gpointer user_data)
 #if 0
     WebKitNetworkProxySettings *proxy_settings = webkit_network_proxy_settings_new ("http://proxy-host-name:12345/", NULL);
     webkit_web_context_set_network_proxy_settings (context, WEBKIT_NETWORK_PROXY_MODE_CUSTOM, proxy_settings);
-    webkit_web_context_set_tls_errors_policy (context, WEBKIT_TLS_ERRORS_POLICY_IGNORE);
 #endif
 
     GtkWidget *webView = webkit_web_view_new_with_context (context);
 
-    // WebKitSettings
-    // API refs: https://webkitgtk.org/reference/webkit2gtk/2.26.2/WebKitSettings.html
+    /* WebKitSettings */
+    /* API refs: https://webkitgtk.org/reference/webkit2gtk/2.26.2/WebKitSettings.html */
     {
         WebKitSettings *settings = webkit_web_view_get_settings (WEBKIT_WEB_VIEW (webView));
+        /* JavaScriptを有効にする */
         webkit_settings_set_enable_javascript (settings, TRUE);
+        /* 2D描画処理のハードウェアアクセラレーション有効にする */
         webkit_settings_set_enable_accelerated_2d_canvas (settings, TRUE);
         webkit_web_view_set_settings (WEBKIT_WEB_VIEW (webView), settings);
     }
